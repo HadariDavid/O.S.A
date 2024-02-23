d47a7ae9025934e784f6da3842d36f443c41288f
Szabadka Konrád <112862066+SzabadkaKonrad@users.noreply.github.com>
2024-02-23 09:27:26 +0100

    Új szak
    A tanár regisztrációs felületen a + gomb sikeresen hozzáad egy új szak text input mezőt és a css formázás is működik (sorközi css)

--- OSA/Frontend/src/scripts.js
+++ OSA/Frontend/src/scripts.js
@@ -1,25 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() { // Megvárja amég az oldal betölt és utána futtatja le a bennelévő scriptet.
+
 // PLUSZ ÉS MINUSZ GOMBOK -------------------
 const plu = document.getElementById('plu'); // plu (PLUsz rövidítés) | HTML -> regist-t.component.html -> [button id] (plu)
 const min = document.getElementById('min'); // min (MINusz rövidítés) | HTML -> regist-t.component.html -> [button id] (min)
 const szakContainer = document.getElementById('szak-container'); // szakContainer | HTML -> regist-t.compoinent.html -> [div id] (szak-container)
-const szakMegadasa = '<input type="text" class="form-control long" placeholder="Új szak megadása" title="Új szak megadása" id="szak-text" required>';
 
 // plusz gomb funkciója
 // (hozzáad egy [új szak megadása] beviteli mezőt )
 function plusButton(){
-    szakContainer.innerHTML = szakMegadasa
-    console.log("PLUS")
+    const ujSzak = document.createElement('input') // létrehoz egy ujSzak nevű inputot.
+    // [ujSzak] input mező paraméterei:
+    ujSzak.type= "text";
+    ujSzak.classList = "form-control ex-long";
+    ujSzak.placeholder = "Új szak megadása";
+    ujSzak.title = "Új szak megadása";
+    ujSzak.id = "szak-text";
+    ujSzak.required = true;
+    ujSzak.maxLength = "128";
+    ujSzak.style = "width: 55%; padding: 10px; margin-bottom: 20px; margin-left: 0.5%; margin-right: 0.5%; border: 3px solid #ffa600; border-radius: 5px;"
+    szakContainer.appendChild(ujSzak);
+    console.log("PLUS");
 }
 
 // minusz gomb funkciója
 // (elvesz egy [új szak megadása] beviteli mezőt )
 function minusButton(){
-    szakContainer.innerHTML = szakMegadasa
     console.log("MINUS")
 }
 
 plu.addEventListener('click', plusButton); // Event Listener a plusz gombhoz (plu)
 min.addEventListener('click', minusButton); // Event Listener a minusz gombhoz (min)
 
-plusButton();
-minusButton();
+});
