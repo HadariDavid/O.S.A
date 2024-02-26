1bca0f3ce8eb660f69291e4aa911dcd2db8536b7
HadariDavid <112862112+HadariDavid@users.noreply.github.com>
2024-02-26 23:40:24 +0100

    Update loginRegisterController.js

--- OSA/Backend/src/controllers/loginRegisterController.js
+++ OSA/Backend/src/controllers/loginRegisterController.js
@@ -2,10 +2,14 @@
 const sequelize = require("../db");
 const bcrypt = require("bcrypt");
 
-const TanarModel = require("../dbModels/tanaradatlap.model");
+const TanaradatlapModel = require("../dbModels/tanaradatlap.model");
 const DiakadatlapModel = require("../dbModels/diakadatlap.model");
 
 
+async function hashData(data){
+    return await bcrypt.hash(data, 13);
+    
+}
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -134,6 +138,10 @@ function registStudent(req, res){
         "response":"sikeres regisztráció"
     });
 
+    /*
+    users.forEach((user) =>{
+        console.log("felhasználó: " + user.name + "; jelszó: " + user.password)
+    });*/
 }
 /////////////////////////////////////////////////////////////////////////////
 
@@ -144,9 +152,10 @@ function registStudent(req, res){
 async function registTeacher(req, res){
 
 var tanarKötAdatok = [
-    "nev",
+    "vezeteknev",
+    "keresztnev",
     "neme",
-    "anyjaNeve",
+    "anyjaleanyneve",
     "szülIdo",
     "szülHely",
     "allampolgar",
@@ -159,11 +168,13 @@ var tanarKötAdatok = [
     "bankszámlaszám",
     "jogviszony",
     "jogKezdete",
-    "szak"
+    "szak",
+    "jelszo"
 ];
 
 for(let i = 0; i< tanarKötAdatok.length; i++){
     if(tanarKötAdatok[i] != Object.keys(req.body)[i] ){
+        console.log(tanarKötAdatok[i] + Object.keys(req.body)[i]);
         res.status(400).json({
             "error": "nem megfelelő adat került elküldésre" 
             });
@@ -183,37 +194,53 @@ Object.values(tanarKötAdatok).forEach((element)=>{
 
 //const jelszo = req.body.jelszo;
 
-// 13 salt egy fél mp kódolást igényel és lefordítást bejelentkezési idő kb 1 mp
-//const hash = await bcrypt.hash(jelszo, 13);
 
 /* visszafordítás
 const isMach = await bcrypt.compare("Password1", hash);
 console.log(isMach);
 */
 
-//hash-elt jelszót feltölteni adatbázisba (várunk ilyen mezőre)
-const tanar1 = TanarModel.build({
-    id:req.body.id,
-    tanarneve: req.body.tanarneve,
-    szakok:req.body.szakok,
-    osztalyok:req.body.osztalyok
-});
-console.log(tanar1.toJSON);
+// 13 salt egy fél mp kódolást igényel és lefordítást bejelentkezési idő kb 1 mp
 
-tanar1.save().then(() => {
+        const tanar1 = TanaradatlapModel.build({
+            id:123456789,
+            osztalyId:"2/14c",
+            csaladNev:req.body.vezeteknev,
+            keresztNev: req.body.keresztnev,
+            szuletesiHely: req.body.szülHely,
+            szuletesiDatum:req.body.szülIdo,
+            szuletesiOrszag:"Magyarország",
+            allampolgarsag:req.body.allampolgar,
+            anyanyelv:req.body.anyanyelv,
+            telefon:"+36325578898",
+            email:"email@email.com",
+            anyjaleanyneve:req.body.anyjaleanyneve,
+            lakcim:req.body.lakcim,
+            taj:req.body.TAJ,
+            adoSzam:req.body.adoSzam,
+            bankszámlaszám:req.body.bankszámlaszám,
+            oktatasiAzonosito:req.body.OMAzon,
+            iskolaAzonosito:12345678911,
+            oraId:123456789,
+            szerep:"Tanar",
+            jogviszonyKezdete:req.body.jogKezdete,
+            jogviszonyVartVege:"2025.08.30",
+            jogviszony:req.body.jogviszony,
+            szakok: req.body.szak,
+            jelszo: await hashData(req.body.jelszo)
+            
+            });
+            console.log(tanar1.toJSON());
 
-     res.status(200).json({
-        "response":"sikeres regisztráció"
-    });
-})
+            tanar1.save().then(() => {
 
-    /*
-    users.forEach((user) =>{
-        console.log("felhasználó: " + user.name + "; jelszó: " + user.password)
-    });*/
-}
+                res.status(200).json({
+                    "response":"sikeres regisztráció"
+                });
+            });
+    }         
+        
 /////////////////////////////////////////////////////////////////////////////
-
 
 
 
