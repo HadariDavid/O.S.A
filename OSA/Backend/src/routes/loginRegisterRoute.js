26e76cfe0eda68fb67816900fd531ae10b5b63b9
HadariDavid <112862112+HadariDavid@users.noreply.github.com>
2024-02-29 17:56:01 +0100

    Update loginRegisterRoute.js

--- OSA/Backend/src/routes/loginRegisterRoute.js
+++ OSA/Backend/src/routes/loginRegisterRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const passport = require("passport");
 const logRegRouter = express.Router();
 const logRegController = require("../controllers/loginRegisterController");
 
 logRegRouter.get("/logout", logRegController.logout);
-logRegRouter.post("/login", logRegController.login);
+logRegRouter.post("/login", passport.authenticate('local',{ failureRedirect: '/', failureMessage: true }), logRegController.login);
 logRegRouter.put("/registration/student",logRegController.registStudent);
 logRegRouter.put("/registration/teacher",logRegController.registTeacher);
 
