d73ed39d84eb7d212bc953e3b1f7383f6b937d06
HadariDavid <112862112+HadariDavid@users.noreply.github.com>
2024-02-29 17:54:35 +0100

    Update passport.config.js

--- OSA/Backend/src/passport.config.js
+++ OSA/Backend/src/passport.config.js
@@ -1,34 +1,68 @@
 const { authenticate } = require("passport");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 const LocalStrategy = require("passport-local").Strategy;
+const diakModel = require("./dbModels/diakadatlap.model");
+const TanarModel = require("./dbModels/tanaradatlap.model");
 
 
-function initialize(passport, getUserByEmail){
 
-    const authenticateFelhasznalo = async (email, jelszo, done) => {
-        const felhasznalo = getUserByEmail(email);
-        if(felhasznalo == null){
-            return done(null, false, {message: "nincs ilyen felhasználó"});
-        }
 
-        try{
-            
-            if(await bcrypt.compare(jelszo, felhasznalo.jelszo)){
-                return done(null, felhasznalo)
-            }else{
-                return done(null, false, {message: "rossz felhasználó"});
+async function initialize(passport){
+
+    const authenticateFelhasznalo = async (email, kapotjelszo, done) => {
+        await TanarModel.findOne({where:{email: email},}).then((felhasznalo) => geci(felhasznalo))}
+           /* felhasznalo = TanarModel.findOne({
+                                where:{
+                                    email: email
+                                },
+                            },
+
+             async function(error, felhasznalo){
+                
+                console.log(felhasznalo);
+                if (error) { return done(error); }
+                if (!felhasznalo) { return done(null, false , {message: "nincs ilyen felhasználó"}); }
+
+                try{ 
+                    if(await bcrypt.compare(jelszo, felhasznalo.jelszo)){
+                        return done(null, felhasznalo);
+                    }else{
+                        return done(null, false, {message: "rossz felhasználó"});
+                    }
+                }catch(error){
+                    return done(error);
+
+                }
+             })
+             */
+
+              
+             
+             async function geci(felhasznalo){
+             if (!felhasznalo){
+                console.log("probléma");
+                return done(null, false, {message:"nincs ilyen felhasználó"});
+             }
+
+             if(await bcrypt.compare(felhasznalo.jelszo,"Kiskutya1")){
+                console.log("jelszó egyezik");
+                return done(null, felhasznalo, {message:"jelszó egyezik"});
+             }else{
+                return done(null, false, {message: "rossz jelszó"});
+             }
             }
 
-        }catch(error){
-            return done(error);
 
-        }
-    };
+    
 
-  passport.use(new LocalStrategy({usernameField:"email", passwordField:"jelszo"}, authenticateFelhasznalo));  
-  passport.serielizeUser((user, done)=> { });
-  passport.deserielizeUser((id , done)=> { });
+  passport.use(new LocalStrategy({usernameField:"email", passwordField:"jelszo"}, authenticateFelhasznalo)); 
+  passport.serializeUser((felhasznalo, done)=> {done(null, felhasznalo.id) }); 
+ // passport.deserializeUser((email, done)=> {done(null, felhasznalo.email) });
+
+
+
 
 }
 
