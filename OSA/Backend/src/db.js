678b3e7e7cc9393e95758180c96f64e082d8578b
HadariDavid <112862112+HadariDavid@users.noreply.github.com>
2024-03-02 16:58:47 +0100

    Delete OSA/Backend/src directory
    nagyobb változtatás miatti törlés

--- OSA/Backend/src/db.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require("sequelize");
-
-const sequelize = new Sequelize("osadbtest", "root", "", {
-    host:"localhost",
-    dialect:"mariadb"
-});
-
-module.exports = sequelize;
