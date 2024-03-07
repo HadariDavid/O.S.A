7787d1999c68fa53e55e4b9f82e0989090180ae2
HadariDavid <112862112+HadariDavid@users.noreply.github.com>
2024-03-07 10:58:24 +0100

    adatmódosítás frissítve működő képes

--- OSA/Backend/src/controllers/adatmodositasController.js
+++ OSA/Backend/src/controllers/adatmodositasController.js
@@ -10,9 +10,6 @@ const { QueryTypes } = require("sequelize");
 
 ///////////////////////////////////////////////////
 
-var szerkFelhasznalo;
-let setModel;
-
 async function getUser(req, res){
 
    TanaradatlapModel.findOne({
@@ -56,39 +53,101 @@ async function getUser(req, res){
 }
 
 async function patchUser(req,res){
-
-    let setThis = Object.keys(req.body);
-    let setData = Object.values(req.body);
     
     try{
-        for(let i = 0; i< setThis.length; i++){
-        sequelize.query("UPDATE :model SET :param = :data WHERE id = :userid",{
-            replacements:{
-                model: setModel,
-                param: setThis[i],
-                data: setData[i],
-                userid: parseInt(szerkFelhasznalo.id)
-            },
-            type:QueryTypes.UPDATE
-        });
-        }
-    }catch(error){
+        TanaradatlapModel.findOne({
+            where:{
+                id: req.body.id
+            }
+        }).then(async (tanar) => {
+                tanar.set({
+                    id:(req.body.csaladNev.substring(0,2) + req.body.keresztNev[0]),
+                    osztalyId:req.body.osztalyId,
+                    csaladNev:req.body.csaladNev,
+                    keresztNev: req.body.keresztNev,
+                    szuletesiHely: req.body.szuletesiHely,
+                    szuletesiDatum:req.body.szuletesiDatum,
+                    szuletesiOrszag:req.body.szuletesiOrszag,
+                    allampolgarsag:req.body.allampolgarsag,
+                    anyanyelv:req.body.anyanyelv,
+                    telefon:req.body.telefon,
+                    email:req.body.email,
+                    anyjaleanyneve:req.body.anyjaleanyneve,
+                    lakcim:req.body.lakcim,
+                    taj:req.body.taj,
+                    adoSzam:req.body.adoSzam,
+                    bankszámlaszám:req.body.bankszámlaszám,
+                    oktatasiAzonosito:req.body.oktatasiAzonosito,
+                    iskolaAzonosito:req.body.iskolaAzonosito,
+                    oraId:req.body.oraId,
+                    szerep:req.body.szerep,
+                    jogviszonyKezdete:req.body.jogviszonyKezdete,
+                    jogviszonyVartVege:req.body.jogviszonyVartVege,
+                    jogviszony:req.body.jogviszony,
+                    szakok: req.body.szakok,
+                })
+
+                    tanar.save().then(()=>{
+                            return res.status(200).json({
+                                status:"succes", 
+                                message: "sikeres frissités"
+                            });
+                        })
+                })
+
+            if ( tanar === null){
+                DiakadatlapModel.findOne({
+                        where:{
+                            id: req.body.id
+                        }
+                    }).then((diak) => {
+                        diak.set({
+                            id:req.body.id, //OM azonosító
+                            osztalyId:req.body.osztalyId, //besoroló function helye,
+                            csaladNev:req.body.csaladNev,
+                            keresztNev: req.body.keresztNev,
+                            szuletesiHely: req.body.szuletesiHely,
+                            szuletesiDatum:req.body.szuletesiDatum,
+                            szuletesiOrszag:req.body.szuletesiOrszag,
+                            allampolgarsag:req.body.allampolgarsag,
+                            anyanyelv:req.body.anyanyelv,
+                            telefon:req.body.telefon,
+                            email:req.body.email,
+                            anyjaleanyneve:req.body.anyjaleanyneve,
+                            gondviseloNeve:req.body.gondviseloNeve,
+                            gondviseloTelefon:req.body.gondviseloTelefon,
+                            gondviseloEmail:req.body.gondviseloEmail,
+                            lakcim:req.body.lakcim,
+                            taj:req.body.taj,
+                            adoSzam:req.body.adoSzam,
+                            bankszámlaszám:req.body.bankszámlaszám,
+                            iskolaAzonosito:req.body.iskolaAzonosito,
+                            oraId:req.body.oraId,
+                            szerep:req.body.szerep,
+                            jogviszonyKezdete:req.body.jogviszonyKezdete,
+                            jogviszonyVartVege:req.body.jogviszonyVartVege,
+                            jogviszony:req.body.jogviszony,
+                            hatranyosHelyzet:req.body.hatranyosHelyzet,
+                            kepesitesek:req.body.kepesitesek,
+                            egyediMegjegyzes: req.body.egyediMegjegyzes,
+                            
+                        })
+                            diak.save().then(()=>{
+                                return res.status(200).json({
+                                    status:"succes", 
+                                    message: "sikeres frissités"
+                                });
+                            })
+                    });
+                    
+            }
+        }catch(error){
         console.log(error);
+        return
     }
-
-
-    szerkFelhasznalo.save().then(() => {
-        return res.status(200).json({
-            status:"succes", 
-            message: "sikeres frissités"
-        })
-   });
-
-  
- 
-    
-
 }
+
+
 
 
 
