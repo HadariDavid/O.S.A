0ccbd7b67def415b23d325866e1c35085b355e85
HadariDavid <112862112+HadariDavid@users.noreply.github.com>
2024-03-24 15:38:14 +0100

    Admin felhasználó hitelesítés
    middleware és JWToken szerkesztése hogy az be tudja azonosítani az admin jogosultságot

--- OSA/Backend/src/middleware/hitelesitesMiddle.js
+++ OSA/Backend/src/middleware/hitelesitesMiddle.js
@@ -8,7 +8,8 @@ const FeketeListaModel = require("../dbModels/feketelista.model");
 
 /////////////////////////////////////////
 
-const {jwtSecretKey, tokenHeaderKey} = require("../app");
+const tokenHeaderKey = "FejedIsMilyenTeGyász";
+const jwtSecretKey = "ARááákEgyeKiALeskelődőSzemed";
 
 
 async function tokenHitelesites(req, res, next){
@@ -59,4 +60,59 @@ async function tokenHitelesites(req, res, next){
 
 }
 
-module.exports = {tokenHitelesites};
+async function adminHitelesites(req, res, next){
+
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        //check for token
+        if (!token) {
+            return res.status(401).json(
+                    {
+                        success: false,
+                        message: "Error!Token was not provided."
+                    }
+                );
+        }
+
+        if(await FeketeListaModel.findOne({where:{token:token}}) !== null){
+           return res.status(401).json({
+                error:true,
+                messagüzenet:"hozzáférés megtagadva (blacklisted)"
+            })
+        }
+            
+        const decodedToken = jwt.verify(token, jwtSecretKey);
+        const {admin} = jwt.decode(token);
+            if(decodedToken && admin !== undefined){
+                if(admin == 1 ){
+                    next();                  
+                }else{
+                    return res.status(401).json({
+                        error:true,
+                        messagüzenet:"hozzáférés megtagadva nem admin"
+                    })
+                }
+
+            }else{
+                return res.status(520).json({
+                    error:true,
+                    message:"hitelesítés közben hiba történt",
+                });
+            }
+        
+
+
+        
+
+    }catch(error){
+        console.log(error);
+        return res.status(401).json({
+            error:true,
+            messagüzenet:"hozzáférés megtagadva"
+        })
+    }
+
+
+}
+
+module.exports = {tokenHitelesites, adminHitelesites};
