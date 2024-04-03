7dd80c46124dff29e1c5fd06d2451646c7d8bf45
SikeIstvan <112862154+SikeIstvan@users.noreply.github.com>
2024-04-03 09:37:26 +0200

    csengetesirend adatokkal

--- OSA/Database/csengetesirend.sql
+++ OSA/Database/csengetesirend.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Feb 02. 09:04
+-- Létrehozás ideje: 2024. Ápr 03. 09:36
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -31,6 +31,19 @@ CREATE TABLE `csengetesirend` (
   `becsengo` time(5) NOT NULL,
   `kicsengo` time(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `csengetesirend`
+--
+
+INSERT INTO `csengetesirend` (`becsengo`, `kicsengo`) VALUES
+('07:45:00.00000', '08:30:00.00000'),
+('08:40:00.00000', '09:25:00.00000'),
+('09:40:00.00000', '10:25:00.00000'),
+('10:35:00.00000', '11:20:00.00000'),
+('11:30:00.00000', '12:15:00.00000'),
+('12:25:00.00000', '13:10:00.00000'),
+('13:15:00.00000', '14:00:00.00000');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
