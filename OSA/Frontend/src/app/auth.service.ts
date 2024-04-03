5b4357b4d3c0d784a8d61266d842f9a4a766bad6
Szabadka Konrád <112862066+SzabadkaKonrad@users.noreply.github.com>
2024-04-03 07:36:17 +0200

    bejelentkezes

--- OSA/Frontend/src/app/auth.service.ts
+++ OSA/Frontend/src/app/auth.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { bejelentkezesObj, registDObj, registTObj } from './felhasznaloAdatokObj';
 import { Subscription } from 'rxjs';
+import { Route, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, protected router:Router) { }
 
   private tanarRegisztracioRout = "http://localhost:3000/registration/teacher"
   private diakRegisztracioRout = "http://localhost:3000/registration/student"
-  private bejelentkezesRoute = "http://localhost:3000/login"
+  private bejelentkezesRoute = "http://localhost:3000/bejelentkezes"
 
   tanarReg(ujtanar: registTObj) :Subscription{
     return this.http.put<registTObj>(this.tanarRegisztracioRout, ujtanar).subscribe((valasz : any) => {
@@ -28,7 +29,14 @@ export class AuthService {
 
   bejelentkezes(diakbej: bejelentkezesObj) :Subscription{
       return this.http.post<bejelentkezesObj>(this.bejelentkezesRoute, diakbej).subscribe((valasz: any) => {
-        console.log
+
+        if(valasz.success){
+          localStorage.setItem('token',valasz.data.token)
+
+          this.router.navigate(['/tanar']);
+        }
+
+        console.log("sikeres bejelentkezes")
       })
   }
 
